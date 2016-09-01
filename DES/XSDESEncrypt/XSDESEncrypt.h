9d6dba98280beebf01e1367d07507d186826c644
Xaofly Sho <love-shao@qq.com>
2016-09-01 19:11:30 +0800

    ADD Help Document

--- DES/XSDESEncrypt/XSDESEncrypt.h
+++ DES/XSDESEncrypt/XSDESEncrypt.h
@@ -10,9 +10,26 @@
 
 @interface XSDESEncrypt : NSObject
 
-/****** 加密 ******/
+/*!
+ DES加密
+ 
+ @param clearText 需要加密的字符串
+ 
+ @param key 加密密钥
+ 
+ @return NSString 加密后的字符串
+ */
 + (NSString *) encryptUseDES:(NSString *)clearText key:(NSString *)key;
-/****** 解密 ******/
+
+/*!
+ DES解密
+ 
+ @param plainText 需要解密的字符串
+ 
+ @param key 解密密钥
+ 
+ @return NSString 解密后的字符串
+ */
 + (NSString *) decryptUseDES:(NSString *)plainText key:(NSString *)key;
 
 
